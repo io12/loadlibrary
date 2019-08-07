d62d5c7c7507a73790e4b9a9eb907cb945590740
Benjamin Levy <blevy@protonmail.com>
2019-08-07 13:19:54 -0400

    Update to use winelib

--- mpclient.c
+++ mpclient.c
@@ -16,6 +16,7 @@
 # define _GNU_SOURCE
 #endif
 
+#include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -39,10 +40,10 @@
 #include <mcheck.h>
 #include <err.h>
 
-#include "winnt_types.h"
-#include "pe_linker.h"
-#include "ntoskernel.h"
-#include "util.h"
+//#include "winnt_types.h"
+//#include "pe_linker.h"
+//#include "ntoskernel.h"
+//#include "util.h"
 #include "hook.h"
 #include "log.h"
 #include "rsignal.h"
@@ -53,217 +54,175 @@
 
 // Any usage limits to prevent bugs disrupting system.
 const struct rlimit kUsageLimits[] = {
-    [RLIMIT_FSIZE]  = { .rlim_cur = 0x20000000, .rlim_max = 0x20000000 },
-    [RLIMIT_CPU]    = { .rlim_cur = 3600,       .rlim_max = RLIM_INFINITY },
-    [RLIMIT_CORE]   = { .rlim_cur = 0,          .rlim_max = 0 },
-    [RLIMIT_NOFILE] = { .rlim_cur = 32,         .rlim_max = 32 },
+	[RLIMIT_FSIZE]  = { .rlim_cur = 0x20000000, .rlim_max = 0x20000000 },
+	[RLIMIT_CPU]    = { .rlim_cur = 3600,       .rlim_max = RLIM_INFINITY },
+	[RLIMIT_CORE]   = { .rlim_cur = 0,          .rlim_max = 0 },
+	[RLIMIT_NOFILE] = { .rlim_cur = 32,         .rlim_max = 32 },
 };
 
 DWORD (* __rsignal)(PHANDLE KernelHandle, DWORD Code, PVOID Params, DWORD Size);
 
 static DWORD EngineScanCallback(PSCANSTRUCT Scan)
 {
-    if (Scan->Flags & SCAN_MEMBERNAME) {
-        LogMessage("Scanning archive member %s", Scan->VirusName);
-    }
-    if (Scan->Flags & SCAN_FILENAME) {
-        LogMessage("Scanning %s", Scan->FileName);
-    }
-    if (Scan->Flags & SCAN_PACKERSTART) {
-        LogMessage("Packer %s identified.", Scan->VirusName);
-    }
-    if (Scan->Flags & SCAN_ENCRYPTED) {
-        LogMessage("File is encrypted.");
-    }
-    if (Scan->Flags & SCAN_CORRUPT) {
-        LogMessage("File may be corrupt.");
-    }
-    if (Scan->Flags & SCAN_FILETYPE) {
-        LogMessage("File %s is identified as %s", Scan->FileName, Scan->VirusName);
-    }
-    if (Scan->Flags & 0x08000022) {
-        LogMessage("Threat %s identified.", Scan->VirusName);
-    }
-    // This may indicate PUA.
-    if ((Scan->Flags & 0x40010000) == 0x40010000) {
-        LogMessage("Threat %s identified.", Scan->VirusName);
-    }
-    return 0;
+	if (Scan->Flags & SCAN_MEMBERNAME) {
+		LogMessage("Scanning archive member %s", Scan->VirusName);
+	}
+	if (Scan->Flags & SCAN_FILENAME) {
+		LogMessage("Scanning %s", Scan->FileName);
+	}
+	if (Scan->Flags & SCAN_PACKERSTART) {
+		LogMessage("Packer %s identified.", Scan->VirusName);
+	}
+	if (Scan->Flags & SCAN_ENCRYPTED) {
+		LogMessage("File is encrypted.");
+	}
+	if (Scan->Flags & SCAN_CORRUPT) {
+		LogMessage("File may be corrupt.");
+	}
+	if (Scan->Flags & SCAN_FILETYPE) {
+		LogMessage("File %s is identified as %s", Scan->FileName, Scan->VirusName);
+	}
+	if (Scan->Flags & 0x08000022) {
+		LogMessage("Threat %s identified.", Scan->VirusName);
+	}
+	// This may indicate PUA.
+	if ((Scan->Flags & 0x40010000) == 0x40010000) {
+		LogMessage("Threat %s identified.", Scan->VirusName);
+	}
+	return 0;
 }
 
 static DWORD ReadStream(PVOID this, ULONGLONG Offset, PVOID Buffer, DWORD Size, PDWORD SizeRead)
 {
-    fseek(this, Offset, SEEK_SET);
-    *SizeRead = fread(Buffer, 1, Size, this);
-    return TRUE;
+	fseek(this, Offset, SEEK_SET);
+	*SizeRead = fread(Buffer, 1, Size, this);
+	return TRUE;
 }
 
 static DWORD GetStreamSize(PVOID this, PULONGLONG FileSize)
 {
-    fseek(this, 0, SEEK_END);
-    *FileSize = ftell(this);
-    return TRUE;
+	fseek(this, 0, SEEK_END);
+	*FileSize = ftell(this);
+	return TRUE;
 }
 
 static PWCHAR GetStreamName(PVOID this)
 {
-    return L"input";
+	return L"input";
 }
 
 // These are available for pintool.
-BOOL __noinline InstrumentationCallback(PVOID ImageStart, SIZE_T ImageSize)
+BOOL __attribute__((noinline)) InstrumentationCallback(PVOID ImageStart, SIZE_T ImageSize)
 {
-    // Prevent the call from being optimized away.
-    asm volatile ("");
-    return TRUE;
+	// Prevent the call from being optimized away.
+	asm volatile ("");
+	return TRUE;
 }
 
 int main(int argc, char **argv, char **envp)
 {
-    PIMAGE_DOS_HEADER DosHeader;
-    PIMAGE_NT_HEADERS PeHeader;
-    HANDLE KernelHandle;
-    SCAN_REPLY ScanReply;
-    BOOTENGINE_PARAMS BootParams;
-    SCANSTREAM_PARAMS ScanParams;
-    STREAMBUFFER_DESCRIPTOR ScanDescriptor;
-    ENGINE_INFO EngineInfo;
-    ENGINE_CONFIG EngineConfig;
-    struct pe_image image = {
-        .entry  = NULL,
-        .name   = "engine/mpengine.dll",
-    };
+	HANDLE KernelHandle;
+	SCAN_REPLY ScanReply;
+	BOOTENGINE_PARAMS BootParams;
+	SCANSTREAM_PARAMS ScanParams;
+	STREAMBUFFER_DESCRIPTOR ScanDescriptor;
+	ENGINE_INFO EngineInfo;
+	ENGINE_CONFIG EngineConfig;
+	HMODULE Module;
+	DWORD res;
 
-    // Load the mpengine module.
-    if (pe_load_library(image.name, &image.image, &image.size) == false) {
-        LogMessage("You must add the dll and vdm files to the engine directory");
-        return 1;
-    }
+	Module = LoadLibrary(TEXT("./engine/mpengine.dll"));
+	if (Module == NULL) {
+		errx(EXIT_FAILURE, "Failed to load mpengine module");
+	}
 
-    // Handle relocations, imports, etc.
-    link_pe_images(&image, 1);
+	__rsignal = (void *) GetProcAddress(Module, "__rsignal");
+	if (__rsignal == NULL) {
+		errx(EXIT_FAILURE, "Failed to resolve mpengine entrypoint");
+	}
 
-    // Fetch the headers to get base offsets.
-    DosHeader   = (PIMAGE_DOS_HEADER) image.image;
-    PeHeader    = (PIMAGE_NT_HEADERS)(image.image + DosHeader->e_lfanew);
+	VOID ResourceExhaustedHandler(int Signal)
+	{
+		errx(EXIT_FAILURE, "Resource Limits Exhausted, Signal %s", strsignal(Signal));
+	}
 
-    // Load any additional exports.
-    if (!process_extra_exports(image.image, PeHeader->OptionalHeader.BaseOfCode, "engine/mpengine.map")) {
-#ifndef NDEBUG
-        LogMessage("The map file wasn't found, symbols wont be available");
-#endif
-    } else {
-        // Calculate the commands needed to get export and map symbols visible in gdb.
-        if (IsDebuggerPresent()) {
-            LogMessage("GDB: add-symbol-file %s %#x+%#x",
-                       image.name,
-                       image.image,
-                       PeHeader->OptionalHeader.BaseOfCode);
-            LogMessage("GDB: shell bash genmapsym.sh %#x+%#x symbols_%d.o < %s",
-                       image.image,
-                       PeHeader->OptionalHeader.BaseOfCode,
-                       getpid(),
-                       "engine/mpengine.map");
-            LogMessage("GDB: add-symbol-file symbols_%d.o 0", getpid());
-            __debugbreak();
-        }
-    }
+	// Install usage limits to prevent system crash.
+	setrlimit(RLIMIT_CORE, &kUsageLimits[RLIMIT_CORE]);
+	setrlimit(RLIMIT_CPU, &kUsageLimits[RLIMIT_CPU]);
+	setrlimit(RLIMIT_FSIZE, &kUsageLimits[RLIMIT_FSIZE]);
+	setrlimit(RLIMIT_NOFILE, &kUsageLimits[RLIMIT_NOFILE]);
 
-    if (get_export("__rsignal", &__rsignal) == -1) {
-        errx(EXIT_FAILURE, "Failed to resolve mpengine entrypoint");
-    }
-
-    EXCEPTION_DISPOSITION ExceptionHandler(struct _EXCEPTION_RECORD *ExceptionRecord,
-            struct _EXCEPTION_FRAME *EstablisherFrame,
-            struct _CONTEXT *ContextRecord,
-            struct _EXCEPTION_FRAME **DispatcherContext)
-    {
-        LogMessage("Toplevel Exception Handler Caught Exception");
-        abort();
-    }
-
-    VOID ResourceExhaustedHandler(int Signal)
-    {
-        errx(EXIT_FAILURE, "Resource Limits Exhausted, Signal %s", strsignal(Signal));
-    }
-
-    setup_nt_threadinfo(ExceptionHandler);
-
-    // Call DllMain()
-    image.entry((PVOID) 'MPEN', DLL_PROCESS_ATTACH, NULL);
-
-    // Install usage limits to prevent system crash.
-    setrlimit(RLIMIT_CORE, &kUsageLimits[RLIMIT_CORE]);
-    setrlimit(RLIMIT_CPU, &kUsageLimits[RLIMIT_CPU]);
-    setrlimit(RLIMIT_FSIZE, &kUsageLimits[RLIMIT_FSIZE]);
-    setrlimit(RLIMIT_NOFILE, &kUsageLimits[RLIMIT_NOFILE]);
-
-    signal(SIGXCPU, ResourceExhaustedHandler);
-    signal(SIGXFSZ, ResourceExhaustedHandler);
+	signal(SIGXCPU, ResourceExhaustedHandler);
+	signal(SIGXFSZ, ResourceExhaustedHandler);
 
 # ifndef NDEBUG
-    // Enable Maximum heap checking.
-    mcheck_pedantic(NULL);
+	// Enable Maximum heap checking.
+	mcheck_pedantic(NULL);
 # endif
 
-    ZeroMemory(&BootParams, sizeof BootParams);
-    ZeroMemory(&EngineInfo, sizeof EngineInfo);
-    ZeroMemory(&EngineConfig, sizeof EngineConfig);
+	ZeroMemory(&BootParams, sizeof BootParams);
+	ZeroMemory(&EngineInfo, sizeof EngineInfo);
+	ZeroMemory(&EngineConfig, sizeof EngineConfig);
 
-    BootParams.ClientVersion = BOOTENGINE_PARAMS_VERSION;
-    BootParams.Attributes    = BOOT_ATTR_NORMAL;
-    BootParams.SignatureLocation = L"engine";
-    BootParams.ProductName = L"Legitimate Antivirus";
-    EngineConfig.QuarantineLocation = L"quarantine";
-    EngineConfig.Inclusions = L"*.*";
-    EngineConfig.EngineFlags = 1 << 1;
-    BootParams.EngineInfo = &EngineInfo;
-    BootParams.EngineConfig = &EngineConfig;
-    KernelHandle = NULL;
+	BootParams.ClientVersion = BOOTENGINE_PARAMS_VERSION;
+	BootParams.Attributes    = BOOT_ATTR_NORMAL;
+	BootParams.SignatureLocation = L"engine";
+	BootParams.ProductName = L"Legitimate Antivirus";
+	EngineConfig.QuarantineLocation = L"quarantine";
+	EngineConfig.Inclusions = L"*.*";
+	EngineConfig.EngineFlags = 1 << 1;
+	BootParams.EngineInfo = &EngineInfo;
+	BootParams.EngineConfig = &EngineConfig;
+	KernelHandle = NULL;
 
-    if (__rsignal(&KernelHandle, RSIG_BOOTENGINE, &BootParams, sizeof BootParams) != 0) {
-        LogMessage("__rsignal(RSIG_BOOTENGINE) returned failure, missing definitions?");
-        LogMessage("Make sure the VDM files and mpengine.dll are in the engine directory");
-        return 1;
-    }
+#if 0
+	asm volatile ("int3");
+#endif
 
-    ZeroMemory(&ScanParams, sizeof ScanParams);
-    ZeroMemory(&ScanDescriptor, sizeof ScanDescriptor);
-    ZeroMemory(&ScanReply, sizeof ScanReply);
+	res = __rsignal(&KernelHandle, RSIG_BOOTENGINE, &BootParams, sizeof BootParams);
+	if (res != 0) {
+		LogMessage("__rsignal(RSIG_BOOTENGINE) returned failure, missing definitions?");
+		LogMessage("Make sure the VDM files and mpengine.dll are in the engine directory");
+		LogMessage("Error code: %#x", res);
+		return 1;
+	}
 
-    ScanParams.Descriptor        = &ScanDescriptor;
-    ScanParams.ScanReply         = &ScanReply;
-    ScanReply.EngineScanCallback = EngineScanCallback;
-    ScanReply.field_C            = 0x7fffffff;
-    ScanDescriptor.Read          = ReadStream;
-    ScanDescriptor.GetSize       = GetStreamSize;
-    ScanDescriptor.GetName       = GetStreamName;
+	ZeroMemory(&ScanParams, sizeof ScanParams);
+	ZeroMemory(&ScanDescriptor, sizeof ScanDescriptor);
+	ZeroMemory(&ScanReply, sizeof ScanReply);
 
-    if (argc < 2) {
-        LogMessage("usage: %s [filenames...]", *argv);
-        return 1;
-    }
+	ScanParams.Descriptor        = &ScanDescriptor;
+	ScanParams.ScanReply         = &ScanReply;
+	ScanReply.EngineScanCallback = EngineScanCallback;
+	ScanReply.field_C            = 0x7fffffff;
+	ScanDescriptor.Read          = ReadStream;
+	ScanDescriptor.GetSize       = GetStreamSize;
+	ScanDescriptor.GetName       = GetStreamName;
 
-    // Enable Instrumentation.
-    InstrumentationCallback(image.image, image.size);
+	if (argc < 2) {
+		LogMessage("usage: %s [filenames...]", *argv);
+		return 1;
+	}
 
-    for (char *filename = *++argv; *argv; ++argv) {
-        ScanDescriptor.UserPtr = fopen(*argv, "r");
+	for (char *filename = *++argv; *argv; ++argv) {
+		ScanDescriptor.UserPtr = fopen(*argv, "r");
 
-        if (ScanDescriptor.UserPtr == NULL) {
-            LogMessage("failed to open file %s", *argv);
-            return 1;
-        }
+		if (ScanDescriptor.UserPtr == NULL) {
+			LogMessage("failed to open file %s", *argv);
+			return 1;
+		}
 
-        LogMessage("Scanning %s...", *argv);
+		LogMessage("Scanning %s...", *argv);
 
-        if (__rsignal(&KernelHandle, RSIG_SCAN_STREAMBUFFER, &ScanParams, sizeof ScanParams) != 0) {
-            LogMessage("__rsignal(RSIG_SCAN_STREAMBUFFER) returned failure, file unreadable?");
-            return 1;
-        }
+		res = __rsignal(&KernelHandle, RSIG_SCAN_STREAMBUFFER, &ScanParams, sizeof ScanParams);
+		if (res != 0) {
+			LogMessage("__rsignal(RSIG_SCAN_STREAMBUFFER) returned failure, file unreadable?");
+			LogMessage("Error code: %#x", res);
+			return 1;
+		}
 
-        fclose(ScanDescriptor.UserPtr);
-    }
+		fclose(ScanDescriptor.UserPtr);
+	}
 
-    return 0;
+	return 0;
 }
