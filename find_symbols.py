9b7d913f3e8ad5bb2ae960f8f7981213c38ab053
Benjamin Levy <blevy@protonmail.com>
2019-08-18 11:35:15 -0400

    dev

--- find_symbols.py
+++ find_symbols.py
@@ -11,10 +11,13 @@ def find_symbols():
     """Find Windows Defender versions compiled with symbols"""
     for exe in glob("*.exe"):
         tmp = tempfile.TemporaryDirectory()
-        subprocess.call(["cabextract", exe, "-d", tmp.name])
-        subprocess.call(
-            ["x86_64-w64-mingw32-nm",
-             path.join(tmp.name, "mpengine.dll")])
+        subprocess.run(["cabextract", exe, "-d", tmp.name])
+        dll_path = path.join(tmp.name, "mpengine.dll")
+        proc = subprocess.run(["exiftool", dll_path])
+        ver = filter(lambda line: "Product Version Number" in line,
+                     proc.stdout.splitlines()).__next__().split(" : ")[1]
+        print(exe, ver)
+        #subprocess.run(["x86_64-w64-mingw32-nm", dll_path])
 
 
 if __name__ == "__main__":
