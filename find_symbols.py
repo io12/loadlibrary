b23f5b20b3b858f5692fb7134e8d182f4b0fe68f
Benjamin Levy <blevy@protonmail.com>
2019-08-18 15:32:27 -0400

    dev

--- find_symbols.py
+++ find_symbols.py
@@ -6,28 +6,36 @@ import tempfile
 from glob import glob
 from os import path
 
+import r2pipe
+
 
 def find_symbols():
     """Find Windows Defender versions compiled with symbols"""
     for exe in glob("*.exe"):
-        tmp = tempfile.TemporaryDirectory()
-        subprocess.run(["cabextract", exe, "-d", tmp.name],
+        r2 = r2pipe.open(exe)
+        syms = r2.cmd("f~sym.")
+        syms = syms.splitlines()
+        syms = len(syms)
+        print(exe, syms)
+
+        #tmp = tempfile.TemporaryDirectory()
+        #subprocess.run(["cabextract", exe, "-d", tmp.name],
                        capture_output=True)
-        dll_path = path.join(tmp.name, "mpengine.dll")
-        proc = subprocess.run(["exiftool", dll_path], capture_output=True)
+        #dll_path = path.join(tmp.name, "mpengine.dll")
+        #proc = subprocess.run(["exiftool", dll_path], capture_output=True)
 
-        exif = proc.stdout.splitlines()
-        ver = filter(lambda line: b"Product Version Number" in line, exif)
+        #exif = proc.stdout.splitlines()
+        #ver = filter(lambda line: b"Product Version Number" in line, exif)
 
-        try:
-            ver = ver.__next__()
-        except StopIteration:
-            continue
+        #try:
+        #    ver = ver.__next__()
+        #except StopIteration:
+        #    continue
 
-        ver = ver.split(b" : ")[1]
-        ver = ver.decode("utf-8")
+        #ver = ver.split(b" : ")[1]
+        #ver = ver.decode("utf-8")
 
-        print(exe, ver)
+        #print(exe, ver)
         #subprocess.run(["x86_64-w64-mingw32-nm", dll_path])
 
 
