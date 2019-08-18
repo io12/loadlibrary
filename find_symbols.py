54820856fe830564bd7bf1e22382ed2e590dbc14
Benjamin Levy <blevy@protonmail.com>
2019-08-18 10:57:55 -0400

    dev

--- find_symbols.py
+++ find_symbols.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
-
 """Find Windows Defender versions compiled with symbols"""
+
+import subprocess
+import tempfile
+from glob import glob
+
+
+def find_symbols():
+    """Find Windows Defender versions compiled with symbols"""
+    for exe in glob("*.exe"):
+        tmp = tempfile.TemporaryDirectory()
+        subprocess.call(["cabextract", exe, "-d", tmp.name])
+
+
+if __name__ == "__main__":
+    find_symbols()
